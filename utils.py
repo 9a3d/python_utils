f97c3c8f29290c2b11a14583dc560eb16fcab6e2
9a3d <43369235+9a3d@users.noreply.github.com>
2018-09-18 10:52:01 +0200

    update: process_file_by_chunk

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def process_file_by_chunk(file_path, proc = lambda chunk: chunk, chunksize = 4096)
     result = None
     with open(file_path, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b''):
+        for chunk in iter(lambda: f.read(chunksize), b''):
             result = proc(chunk)
     return result
